894fd95b3c576d69701818e89f36c8e620a501b2
Omtech9119 <50166112+Omtech9119@users.noreply.github.com>
2023-12-20 18:32:18 +0530

    Install body-parser

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
@@ -8,7 +9,15 @@ app.listen(3000, () => {
 // serve your css as static
 app.use(express.static(__dirname));
 
+// get our app to use body parser 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get("/", (req, res) => {
     // res.send("Hello world!");
     res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body)
+    // res.send("Thank you for subscribing");
 });
